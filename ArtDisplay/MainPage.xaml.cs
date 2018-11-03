a3e369bb2dc956b1080aed1777cf91c4e674c093
Martin Mandak <mandak@windowslive.com>
2018-11-04 00:29:12 +0100

    .

---
+++ ArtDisplay/MainPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Sensors;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Pickers;
+using Windows.System.Display;
+using Windows.System.Threading;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ArtDisplay
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Settings _settings;
+        private bool _monitorOn = true;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadSettingsAsync();
+            SetSource();
+            DisplayTrigger();
+        }
+
+        private void DisplayTrigger()
+        {
+            if (!_settings.AutoSleep) return;
+
+            TimeSpan period = TimeSpan.FromSeconds(1);
+
+            ThreadPoolTimer PeriodicTimer = ThreadPoolTimer.CreatePeriodicTimer((source) =>
+            {
+                var currentTime = DateTime.Now;
+                var onH = _settings.GetHour(_settings.TurnOnMonitor);
+                var onM = _settings.GetMinutes(_settings.TurnOnMonitor);
+                var offH = _settings.GetHour(_settings.TurnOffMonitor);
+                var offM = _settings.GetMinutes(_settings.TurnOffMonitor);
+
+                if ((currentTime.Hour > onH && currentTime.Hour < offH) ||
+                    (currentTime.Hour == onH && currentTime.Minute >= onM) ||
+                    (currentTime.Hour == offH && currentTime.Minute < offM))
+                {
+                    if (!_monitorOn)
+                    {
+                        _monitorOn = true;
+                        Dispatcher.RunAsync(CoreDispatcherPriority.High,
+                              () =>
+                              {
+                                  // TODO zapni display powershell script
+
+                              });
+                    }
+                }
+                else
+                {
+                    if (_monitorOn)
+                    {
+                        _monitorOn = false;
+                        Dispatcher.RunAsync(CoreDispatcherPriority.High,
+                        () =>
+                        {
+                            //
+                            // TODO vypne display powershell script
+                            //
+                        });
+                    }
+                }
+
+
+            }, period);
+
+
+        }
+
+        private async void LoadSettingsAsync()
+        {
+            var storageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/settings.json"));
+            if (storageFile == null) throw new Exception("Setting file missing!");
+            string contents = await FileIO.ReadTextAsync(storageFile);
+            _settings = JsonConvert.DeserializeObject<Settings>(contents);
+        }
+
+        public async void SetSource()
+        {
+            StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(_settings.DisplayFolder);
+
+            if (folder == null) return;
+
+            StorageApplicationPermissions.FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+
+            IReadOnlyList<StorageFile> sortedItems = await folder.GetFilesAsync();
+
+            if (!sortedItems.Any()) return;
+
+            using (Windows.Storage.Streams.IRandomAccessStream fileStream = await sortedItems[0].OpenAsync(Windows.Storage.FileAccessMode.Read))
+            {
+                // Set the image source to the selected bitmap.
+                BitmapImage bitmapImage = new BitmapImage();
+
+                bitmapImage.SetSource(fileStream);
+                DisplayImage.Source = bitmapImage;
+            }
+        }
+    }
+}
---
+++ ArtDisplay/Settings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtDisplay
+{
+    public class Settings
+    {
+        public string DisplayFolder { get; set; }
+        public string FavoritesFolder { get; set; }
+        public bool AutoSleep { get; set; }
+        public string TurnOnMonitor { get; set; }
+        public string TurnOffMonitor { get; set; }
+        public bool SlideShow { get; set; }
+        public int SlideShowMinutes { get; set; }
+
+
+        public int GetHour(string time)
+        {
+            var result = 0;
+            if (string.IsNullOrWhiteSpace(time)) return result;
+
+            int.TryParse(time.Split(':')[0], out result);
+
+            return result;
+        }
+
+        public int GetMinutes(string time)
+        {
+            var result = 0;
+            if (string.IsNullOrWhiteSpace(time)) return result;
+
+            int.TryParse(time.Split(':')[1], out result);
+
+            return result;
+        }
+    }
+}
